19c0bb697e448e13e78ef5adcfdb4f59ad887723
9YOHAN8 <160392746+9YOHAN8@users.noreply.github.com>
2024-03-22 19:20:06 +0900

    에너미 순찰

---
+++ Source/LostArk/Enemy/Enemy.h
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Enemy.generated.h"
+
+UCLASS()
+class LOSTARK_API AEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AEnemy();
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+
+
+
+
+	
+private:
+
+
+	// 대상
+	UPROPERTY()
+	AActor* CombatTarget;
+
+	//이동속도
+	UPROPERTY(EditAnywhere)
+	double CombatRadius = 500.f; 
+
+	/** 
+	*Navigation 
+	*/
+
+	UPROPERTY()
+	class AAIController* EnemyController;  //액터가 지시사항 전달 컨트롤러에 변수설정
+
+	// 현재 순찰 대상, 배열로 갖게될것임
+	UPROPERTY(EditInstanceOnly, Category = "AI Navigation")
+	AActor* PatrolTarget;  
+
+	UPROPERTY(EditInstanceOnly, Category = "AI Navigation")
+	TArray<AActor*> PatrolTargets;
+
+	// 전투반경
+	UPROPERTY(EditAnywhere)
+	double PatrolRadius = 200.f;
+
+	FTimerHandle PatrolTimer;  // 순찰 타이머
+	void PatrolTimerFinished(); // 순찰 타이머가 끝나면 실행
+
+
+
+
+
+
+
+
+
+protected:
+	virtual void BeginPlay() override;
+
+	// 타깃이 범위내에 있다면 true값 반환     수용반경
+	bool InTargetRange(AActor* Target, double Radius);
+	void MoveToTarget(AActor* Target);   //순찰시간끝나면 이동
+	AActor* ChoosePatrolTarget(); //새 표적 선택하기
+
+
+
+
+
+public:	
+	
+	
+
+
+};
